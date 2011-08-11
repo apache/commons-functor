fa3d4d36c963924b03bb9f88050438b7339690c2
Simone Tripodi <simonetripodi@apache.org>
2011-08-11 20:13:34 +0000

    fixed checkstyle violations: Expression can be simplified
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/functor/trunk@1156802 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/functor/generator/WhileGenerate.java
+++ src/main/java/org/apache/commons/functor/generator/WhileGenerate.java
@@ -75,7 +75,7 @@ public class WhileGenerate<E> extends BaseGenerator<E> {
         if (obj == this) {
             return true;
         }
-        if (obj instanceof WhileGenerate<?> == false) {
+        if (!(obj instanceof WhileGenerate<?>)) {
             return false;
         }
         WhileGenerate<?> other = (WhileGenerate<?>) obj;
